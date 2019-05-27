b274d0296190ce7064f01c42d7436173e649bcd4
pcollinsTech <phil@pcollins.tech>
2019-05-27 22:03:32 +0100

    Products Component updated

--- src/App.js
+++ src/App.js
@@ -1,44 +1,18 @@
 import React, { Component } from 'react'
-import axios from 'axios';
 
 import Products from './components/Products'
 import Upload from './components/Upload'
 import './App.css'
 
 class App extends Component {
-  constructor(){
-    super()
-    this.state = {
-      products: []
-    }
-  }
-
-  componentDidMount() {
-    this.getProducts()
-  }
-
-  getProducts() {
-    return axios.get('http://rlaapi-env.pnqpme3byb.us-east-2.elasticbeanstalk.com/api/products')
-      .then(res => {
-        this.setState({
-          ...this.state,
-          products: res.data
-        })
-      })
-  }
-
-  // componentDidUpdate() {
-  //   this.getProducts()
-  // }
-
+ 
   render() {
-    const {products} = this.state
     return (
       <div className="App">
         <div className="Card">
           <Upload />
         </div>
-        <Products products={products} />
+        <Products />
 
       </div>
     )
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,28 +1,97 @@
-import React from 'react'
+import React, {Component} from 'react'
+import axios from 'axios'
 import './style.css'
+import PropTypes from 'prop-types';
 
-export default ({products}) => {
-  console.log("Products.js",products)
-  return (
-    <table className="table mt-5">
-      <tr >
-        <th scope="col">Category</th>
-        <th scope="col">Sub Category</th>
-        <th scope="col">Part Number</th>
-        <th scope="col">Description</th>
-      </tr>
-        {products.map(product => {
-          return (
-            <tr>
-              <td>{product.sub_category.name}</td>
-              <td>{product.category.name}</td>
-              <td>{product.part_number}</td>
-              <td>{product.description}</td>
+class Products extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      search:'',
+      products: []
+    }
+    console.log(this.props)
+  }
+
+  componentDidMount() {
+    console.log("hi")
+    return this.getProducts()
+  }
+
+  getProducts() {
+    console.log("get")
+    return axios.get('http://rlaapi-env.pnqpme3byb.us-east-2.elasticbeanstalk.com/api/products')
+      .then(res => {
+        console.log("res", res)
+        this.setState({
+          ...this.state,
+          search: "",
+          products: res.data
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
+  updateSearch(event) {
+    this.setState({
+      search: event.target.value.substr(0, 20)
+    })
+  }
+
+  render() {
+
+    const { products } = this.state
+    console.log("HIHI", products)
+    let filterProducts = this.state.products.filter(product => {
+      return product.part_number.toLowerCase().includes(this.state.search.toLowerCase()) ||
+        product.description.toLowerCase().includes(this.state.search.toLowerCase()) ||
+        product.category.name.toLowerCase().includes(this.state.search.toLowerCase()) ||
+        product.sub_category.name.toLowerCase().includes(this.state.search.toLowerCase())
+    })
+    
+    return (
+      <div className="container">
+        <form className="form-inline mt-5">
+          <div className="form-group mb-2 mx-auto ">
+            <label className="mr-2">Search</label>
+            <input
+              type="text"
+              value={this.state.search}
+              onChange={this.updateSearch.bind(this)} />
+          </div>
+        </form>
+        
+        <table className="table mt-5">
+          <thead>
+            <tr >
+              <th scope="col">Category</th>
+              <th scope="col">Sub Category</th>
+              <th scope="col">Part Number</th>
+              <th scope="col">Description</th>
             </tr>
-          )}
-        )}
-    </table>
-// 
-    // 
-  )
+          </thead>
+          <tbody>
+            {filterProducts.map((product, i) => {
+              return (
+                <tr key={i}>
+                  <td>{product.sub_category.name}</td>
+                  <td>{product.category.name}</td>
+                  <td>{product.part_number}</td>
+                  <td>{product.description}</td>
+                </tr>
+              )
+            }
+            )}
+          </tbody>
+        </table>
+
+      </div>
+    )
+  }
 }
+
+Products.propTypes = {
+  products: PropTypes.array,
+}
+
+export default Products
